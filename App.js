2a0aed1bfe328cdfa76ad55729b93ea025033272
Vatsal <vatsalpatel1073@gmail.com>
2021-03-02 14:50:37 +0530

    started navigation

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import { StyleSheet, Text, View } from "react-native";
+import Login from "./Screens/Login";
+
+const Stack = createStackNavigator();
+
+const globalScreen = {
+  headerStyle: { backgroundColor: "#2C68ED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Signal Clone</Text>
-      <StatusBar style='auto' />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreen}>
+        <Stack.Screen name='Login' component={Login} />
+      </Stack.Navigator>
+
+      <View style={styles.container}>
+        <Text>Signal</Text>
+        <StatusBar style='auto' />
+      </View>
+    </NavigationContainer>
   );
 }
 
---
+++ Utils/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB9bw6HqW1yRYWkosGXYh0Wv0WYIjUsQFA",
+  authDomain: "signal-c2f08.firebaseapp.com",
+  projectId: "signal-c2f08",
+  storageBucket: "signal-c2f08.appspot.com",
+  messagingSenderId: "11893325795",
+  appId: "1:11893325795:web:b1b184e965ef14221faad6",
+  measurementId: "G-X4P6BF6TE3",
+};
